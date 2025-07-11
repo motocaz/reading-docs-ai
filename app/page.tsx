584b8178bca877c62a5d080595d6851383be4b89
motocaz <gustavomottadeveloper@gmail.com>
2025-07-11 02:30:05 -0300

    refactor: remove session check and redirect from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { getServerSession } from "next-auth"
-import { redirect } from "next/navigation"
 import { HomePage } from "@/components/home-page"
 
 export default async function Home() {
-  const session = await getServerSession()
-
-  if (!session) {
-    redirect("/auth/signin")
-  }
-
   return <HomePage />
 }
